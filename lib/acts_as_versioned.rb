4275a4a742b70def672e6aa73aaa4d1b48b0cccc
Greg Braman <bramanga@gmail.com>
2012-05-22 10:10:45 -0400

    Fix for mongomapper 11

--- lib/acts_as_versioned.rb
+++ lib/acts_as_versioned.rb
@@ -1,41 +1,41 @@
+require 'active_support/concern'
+
 module MongoMapper
   module Acts
     module Versioned
       extend ActiveSupport::Concern
 
-      VERSION   = '0.1.0'
+      VERSION   = '0.1.1'
       CALLBACKS = [:save_version, :clear_old_versions]
 
-      def self.configure(model)
-        model.class_eval do
-          cattr_accessor :versioned_class_name,
-            :non_versioned_keys, :max_version_limit
+      included do
+        cattr_accessor :versioned_class_name,
+          :non_versioned_keys, :max_version_limit
 
-          self.versioned_class_name = :Version
-          self.max_version_limit    = 0
-          self.non_versioned_keys   = %w[
-            _id _type created_at updated_at
-            creator_id updater_id version
-          ]
+        self.versioned_class_name = :Version
+        self.max_version_limit    = 0
+        self.non_versioned_keys   = %w[
+          _id _type created_at updated_at
+          creator_id updater_id version
+        ]
 
-          const_set(versioned_class_name, Class.new).class_eval do
-            include MongoMapper::EmbeddedDocument
+        const_set(versioned_class_name, Class.new).class_eval do
+          include MongoMapper::EmbeddedDocument
 
-            key :version,  Integer
-            key :modified, Hash
-          end
-
-          class_name = "#{self}::#{versioned_class_name}"
-          many :versions, :class => class_name.constantize do
-            def [](version)
-              detect { |doc| doc.version.to_s == version.to_s }
-            end
-          end
-
-          key :version, Integer
-          before_save :save_version
-          before_save :clear_old_versions
+          key :version,  Integer
+          key :modified, Hash
         end
+
+        class_name = "#{self}::#{versioned_class_name}"
+        many :versions, :class => class_name.constantize do
+          def [](version)
+            detect { |doc| doc.version.to_s == version.to_s }
+          end
+        end
+
+        key :version, Integer
+        before_save :save_version
+        before_save :clear_old_versions
       end
 
       module InstanceMethods
