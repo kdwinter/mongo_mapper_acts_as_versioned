78d89e7a9ad1db1eb76b73f11bdf38db24b1ce66
Greg Braman <bramanga@gmail.com>
2013-06-28 11:05:01 -0400

    Update version number

--- lib/acts_as_versioned.rb
+++ lib/acts_as_versioned.rb
@@ -5,7 +5,7 @@ module MongoMapper
     module Versioned
       extend ActiveSupport::Concern
 
-      VERSION   = '0.1.3'
+      VERSION   = '0.1.4'
       CALLBACKS = [:save_version, :clear_old_versions]
 
       included do
